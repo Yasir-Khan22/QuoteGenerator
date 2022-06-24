3a5001c0b00d2f403e86ee8f8e9fa784588cd734
Yasir Khan <yasirkhanone22@gmail.com>
2022-06-24 23:01:12 +0500

    files are changed

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ function randomQuote() {
   fetch("https://api.quotable.io/random")
     .then((res) => res.json())
     .then((result) => {
+      console.log(result);
       quoteText.innerText = result.content;
       authorName.innerText = result.author;
     });
